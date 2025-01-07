969f565aa0b72f58a7b41743d4cbed40f3fcbe8a
FrankIllner <frankillner@web.de>
2025-01-07 20:44:02 +0100

    DNS eingerichtet

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
-  'https://frontend-kiezjagd.vercel.app' ,
+  'https://www.kiezjagd.de',
+  'https://frontend-kiezjagd.vercel.app',
   'http://localhost:8080', 
 ];
 
