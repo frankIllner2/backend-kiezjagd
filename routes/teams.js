f1b9eb9bc8d331cb1cee8f02fa26cefabc4163ec
FrankIllner <frankillner@web.de>
2025-01-14 15:57:53 +0100

    checkTeamname

--- routes/teams.js
+++ routes/teams.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const Teams = require('../models/Teams');
+const Result = require('../models/Result');
 
-// Teamname prüfen
 router.get('/check', async (req, res) => {
-  const { teamName } = req.query;
-  if (!teamName) {
-    return res.status(400).json({ message: 'Teamname erforderlich.' });
+  const { teamName, gameId } = req.query;
+  console.log('checkname');
+  console.log(teamName);
+  console.log(gameId);
+  if (!teamName || !gameId) {
+    return res.status(400).json({ message: 'Teamname und Spiel-ID sind erforderlich' });
   }
 
-  const exists = await Teams.exists({ name: teamName });
-  res.status(200).json({ exists });
+  try {
+    // Prüfe, ob ein Team mit diesem Namen und Spiel-ID existiert
+    const existingTeam = await Result.findOne({ teamName, gameId });
+    if (existingTeam) {
+      return res.json({ exists: true });
+    }
+    res.json({ exists: false });
+  } catch (error) {
+    console.error('Fehler bei der Überprüfung des Teamnamens:', error.message);
+    res.status(500).json({ message: 'Interner Serverfehler', error: error.message });
+  }
 });
+
 
 module.exports = router;
