d3fb7016059b2efb450f285b2445d9d30e11d1e8
FrankIllner <frankillner@web.de>
2025-08-27 07:18:07 +0200

    Voucher name anzeigen speichern

--- models/Game.js
+++ models/Game.js
@@ -37,6 +37,7 @@ const GameSchema = new mongoose.Schema({
   infohistory: { type: String, required: false },
   isDisabled: { type: Boolean, default: false },
   isVoucher: { type: Boolean, default: false },
+  voucherName: { type: String, required: false },
   gameImage: { type: String, required: true }, 
   playtime: { type: String, required: false },
   startloction: { type: String, required: true },
