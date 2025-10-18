821d63f23f2fbfe42acf7f84c0626a2d6d3ea53b
FrankIllner <frankillner@web.de>
2025-10-18 10:37:34 +0200

    GPS Bug

--- routes/games.js
+++ routes/games.js
@@ -192,7 +192,7 @@ router.post("/:encryptedId/verify-location", async (req, res) => {
       question.coordinates.lat, question.coordinates.lon
     );
 
-    if (distance <= 15) {
+    if (distance <= 20) {
       res.json({ success: true, message: "Standort korrekt!" });
     } else {
       res.json({ success: false, message: "Zu weit entfernt!" });
